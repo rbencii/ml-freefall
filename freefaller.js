6a6b1328eda41eb4245324ed4be552ebddd7f039
rbencii <83843622+rbencii@users.noreply.github.com>
2022-09-15 23:23:10 +0200

    Updated FreeFaller's animate function

--- freefaller.js
+++ freefaller.js
@@ -4,22 +4,32 @@ class FreeFaller {
         this.y=y;
         this.w=80;
         this.h=50;
-        this.acceleration=2;
+        this.acceleration=3;
+        this.speed=2;
+        this.maxSpeed=8;
+        this.minSpeed=2;
         this.controls=new Controls();
     }
 
     animate(){
         if(this.controls.left)
             this.x-=2;
+
         if(this.controls.right)
             this.x+=2;
+
         if(this.controls.headDown)
-            this.acceleration+=2
-        else if(this.acceleration>2)
-            this.acceleration-=2
-        if(this.acceleration>16)
-            this.acceleration=16
-        this.y+=this.acceleration
+            this.speed+=this.acceleration;
+        else if(this.speed>2)
+            this.speed=Math.max(this.speed-this.acceleration,this.minSpeed);
+        
+
+        if(this.speed>this.maxSpeed)
+            this.speed=this.maxSpeed;
+
+        this.y+=this.speed
+        if(this.y>100)
+            this.y-=this.acceleration;
     }
     
     draw(ctx){
