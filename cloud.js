e07ca329d045e10174f1fd295a336bd74cb7ac74
rbencii <83843622+rbencii@users.noreply.github.com>
2022-09-16 01:05:33 +0200

    Added a cloud into frame's animate cycle

--- cloud.js
+++ cloud.js
@@ -4,8 +4,17 @@ class Cloud {
         this.y=y;
         this.w=Math.floor(Math.random()*101+100);
         this.h=50;
+        this.minSpeed=2;
     }
     
+    animate(dt){
+        this.y-=this.minSpeed*dt;
+    }
+
+    isDead(){
+        return this.y<0;
+    }
+
     draw(ctx){
         ctx.fillRect(this.x-this.w/2.0,this.y-this.h/2.0,this.w,this.h)
     }
--- frame.js
+++ frame.js
@@ -13,13 +13,18 @@ const perfectFrameTime = 1000 / 60;
 let deltaTime = 0;
 let lastFrame = 0;
 
+let testCloud = new Cloud(Math.floor(Math.random()*(gameCanvas.width-200)+100),gameCanvas.height);
+
 function animate(){
     let currentFrame = performance.now();
     deltaTime = (currentFrame - lastFrame) / perfectFrameTime;
     lastFrame = currentFrame;
-
+    if(testCloud.isDead)
+        testCloud = new Cloud(Math.floor(Math.random()*(gameCanvas.width-200)+100),gameCanvas.height);
+    testCloud.animate(deltaTime);
     testFaller.animate(deltaTime);
     gameCanvas.height=window.innerHeight;
+    testCloud.draw(gameContext)
     testFaller.draw(gameContext)
     requestAnimationFrame(animate);
 }
